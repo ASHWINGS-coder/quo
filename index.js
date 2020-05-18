18abaeeaa1a31a8bbc45b74274bcf23b9e714cfd
Ash <ashwings1996@gmail.com>
2020-05-18 11:42:34 +0530

    set up static files and layouts

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express'); // requiring express
 const app = express(); // using express
 const port = 3000; // initialising port
+const expressLayouts = require('express-ejs-layouts');//importing ejs libaray
 
+app.use(express.static('./assets'));// setting up static files
+
+app.use(expressLayouts);// using expresslayouts
+// extract styles and scripts from sub pages into layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+// set up the  view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 // use express router
 app.use('/',require('./routes'));
