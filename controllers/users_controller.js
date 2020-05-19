7f6834c6abd5b781e8a20790085e317c38e235ad
Ash <ashwings1996@gmail.com>
2020-05-19 14:56:35 +0530

    add sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user'); // import user model
+
 module.exports.profile = function(req,res){
     res.render('user_profile',{
         title:"profile"
@@ -19,7 +21,29 @@ module.exports.signin = function(req,res){
 
 // get the signup data 
 module.exports.create =  function(req,res){
-    // todo later
+  if(req.body.password != req.body.confirm_password){
+      return res.redirect('back');
+  }
+
+  User.findOne({email:req.body.email},function(err,user){
+      if(err){
+          console.log('error in finding user in signing up');
+          return;
+      }
+
+      if(!user){
+          User.create(req.body,function(err,user){
+            if(err){
+                console.log('error in creating user in signing up');
+                return;
+            }
+            return res.redirect('/users/sign-in');
+          })
+      }else{
+          return res.redirect('back');
+      }
+  })
+
 }
 
 // sign in and create a session for the user
