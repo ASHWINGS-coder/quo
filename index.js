90aafa65460d6ae88919310b4dc743fdac13655b
Ash <ashwings1996@gmail.com>
2020-05-21 11:02:27 +0530

    add monog store

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const session = require('express-session'); // using expression session
 const passport = require('passport'); // using passport
 // importing local strategy
 const passportLocal = require('./config/passport-local-strategy');
+ // importing connect-mongo to store session cookies
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded()); // using url encoded
 app.use(cookieParser()); // using cookie-parser
@@ -25,6 +27,7 @@ app.set('view engine','ejs');
 app.set('views','./views');
 
 // using express session (stored in session const)
+// monogostore is used to store the session cookie in db
 app.use(session({
     name:'codeial',
     secret:'blahsomething',
@@ -32,7 +35,13 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000*60*100)
-    }
+    },
+    store:new MongoStore({
+        mongooseConnection:db,
+        autoRemove:'disabled'
+    },function(err){
+        console.log(err || 'connect mongo db set up')
+    })
 }));
  // saying app to use passport initialse is a function 
 app.use(passport.initialize());
