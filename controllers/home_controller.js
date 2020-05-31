16f8b21d46460a1bcc7d92aaa9e5f58954f7fed4
Ash <ashwings1996@gmail.com>
2020-05-31 14:48:42 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,26 +1,28 @@
 const Post = require('../models/posts');
 const User = require('../models/user');
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // console.log(req.cookies)
     // res.cookie('user_id',21)
+    try{
+            let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path:'comments',
+                populate:{
+                    path: 'user'
+                }
+            })
+        let users = await  User.find({});
+        return res.render('home',{
+            title:"Q&A  ",
+            posts:posts,
+            all_users:users
+        });
+    }catch(err){
+        console.log("Error",err);
+        return ;
+    }
 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title:"Q&A  ",
-                posts:posts,
-                all_users:users
-            })})
-        })
-            
 
     // Post.find({},function(err,post){
     //     return res.render('home',{
