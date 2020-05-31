03a276d8b2c61465c73a3550c2236034eeba0fff
Ash <ashwings1996@gmail.com>
2020-05-31 17:50:28 +0530

    ahax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,11 +3,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
     try{
-        await  Post.create({
+       let post = await  Post.create({
             content:req.body.content,
             user:req.user._id
         });
-        return res.redirect('back');
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post:post
+                    },
+                    message:"Post created"
+                })
+            }
+            return res.redirect('back');
     }catch(err){
         console.log('Error',err);
         return;
@@ -25,6 +34,15 @@ module.exports.destroy = async function(req,res){
             post.remove();
     
             await Comment.deleteMany({post:req.body.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:"Post deleted"
+                })
+            }
             return res.redirect('back')
         }else{
             return res.redirect('back')
